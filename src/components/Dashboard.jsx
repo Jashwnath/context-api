ebad650300edbbd22373f48f24516026e83e173c
Jashwanth Alwaru <alwarujashwanth@gmail.com>
2024-03-01 21:48:19 +0530

    Adding the context api

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,5 @@
+export default  function Dashboard(){
+return <div>
+    jash dashboard
+</div>
+}
---
+++ src/components/Landing.jsx
@@ -0,0 +1,5 @@
+export  default function Landing(){
+    return <div>
+            jsh landing page
+         </div>
+    }
